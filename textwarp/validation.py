e5ab6d780c39fd56d5a00a09cacb2eaf41874fac
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-10 01:51:22 -0400

    Create ClipboardError and NonTextClipboardError classes

--- textwarp/validation.py
+++ textwarp/validation.py
@@ -1,2 +1,13 @@
-class EmptyClipboardError(Exception):
+class ClipboardError(Exception):
+    """Base class for clipboard exceptions."""
+    pass
+
+
+class EmptyClipboardError(ClipboardError):
     """Exception raised when the clipboard is empty."""
+
+
+class NonTextClipboardError(ClipboardError):
+    """Exception raised when the clipboard content is not text."""
+    pass
+
