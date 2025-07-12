7adea7d8a6e90fbfab57fd13e2b11cd014142408
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-12 08:59:42 -0400

    Create config.py

---
+++ textwarp/config.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+# Get the directory of the current file.
+file_dir: str = os.path.dirname(__file__)
+
+# Construct a platform-independent path to the JSON file.
+json_file_path: str = os.path.join(
+    file_dir, 'data', 'contractions_map.json'
+)
+
+# Load the JSON data, which pairs a contraction with its expanded
+# version.
+with open(json_file_path, 'r') as json_file:
+    contractions_dict: dict[str, str] = json.load(json_file)
