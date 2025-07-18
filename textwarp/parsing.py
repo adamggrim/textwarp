a944c3ad3a07e528ce5bf418a364b428803b1be2
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-18 02:22:52 -0400

    Refactor parse_args() to eliminate excessive repetition

--- textwarp/parsing.py
+++ textwarp/parsing.py
@@ -2,32 +2,8 @@ import argparse
 import sys
 from typing import Callable
 
-from textwarp.constants import HelpMessages
-from textwarp.warping import (
-    capitalize,
-    cardinal_to_ordinal,
-    curly_to_straight,
-    expand_contractions,
-    hyphens_to_em,
-    hyphen_to_en,
-    punct_to_inside,
-    ordinal_to_cardinal,
-    punct_to_outside,
-    straight_to_curly,
-    strikethrough,
-    to_alternating_caps,
-    to_binary,
-    to_camel_case,
-    to_hexadecimal,
-    to_kebab_case,
-    to_lowercase,
-    to_pascal_case,
-    to_sentence_case,
-    to_single_spaces,
-    to_snake_case,
-    to_title_case,
-    to_uppercase
-)
+from textwarp.args import ARGS_MAP
+from textwarp.constants import HELP_DESCRIPTION
 
 
 def parse_args() -> Callable[[str], str]:
@@ -38,70 +14,33 @@ def parse_args() -> Callable[[str], str]:
         Callable[[str], str]: The text warping function corresponding
             to the specified command-line argument.
     """
+    max_arg_width = _calculate_max_arg_width(ARGS_MAP)
+
     # A custom help formatter to align help messages neatly based on
     # the maximum argument length.
     formatter: Callable[[str], argparse.HelpFormatter] = (
-        lambda prog: argparse.HelpFormatter(prog, max_help_position=25)
+        lambda prog: argparse.HelpFormatter(
+            prog, max_help_position=max_arg_width
+        )
     )
 
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
         prog='textwarp',
         formatter_class=formatter,
-        description=HelpMessages.DESCRIPTION,
+        description=HELP_DESCRIPTION,
         usage='%(prog)s [command]'
     )
 
     group: argparse._MutuallyExclusiveGroup = (
         parser.add_mutually_exclusive_group(required=True)
     )
-    group.add_argument('--alternating-caps', action='store_true',
-                       help=HelpMessages.ALTERNATING_CAPS)
-    group.add_argument('--binary', action='store_true',
-                       help=HelpMessages.BINARY)
-    group.add_argument('--camel-case', action='store_true',
-                       help=HelpMessages.CAMEL_CASE)
-    group.add_argument('--capitalize', action='store_true',
-                       help=HelpMessages.CAPITALIZE)
-    group.add_argument('--cardinal', action='store_true',
-                       help=HelpMessages.CARDINAL)
-    group.add_argument('--curly-quotes', action='store_true',
-                       help=HelpMessages.CURLY_QUOTES)
-    group.add_argument('--expand-contractions', action='store_true',
-                       help=HelpMessages.EXPAND_CONTRACTIONS)
-    group.add_argument('--hexadecimal', action='store_true',
-                       help=HelpMessages.HEXADECIMAL)
-    group.add_argument('--hyphens-to-em', action='store_true',
-                       help=HelpMessages.HYPHENS_TO_EM)
-    group.add_argument('--hyphen-to-en', action='store_true',
-                       help=HelpMessages.HYPHEN_TO_EN)
-    group.add_argument('--kebab-case', action='store_true',
-                       help=HelpMessages.KEBAB_CASE)
-    group.add_argument('--lowercase', action='store_true',
-                       help=HelpMessages.LOWERCASE)
-    group.add_argument('--ordinal', action='store_true',
-                       help=HelpMessages.ORDINAL)
-    group.add_argument('--pascal-case', action='store_true',
-                       help=HelpMessages.PASCAL_CASE)
-    group.add_argument('--punct-to-inside', action='store_true',
-                       help=HelpMessages.PUNCT_TO_INSIDE)
-    group.add_argument('--punct-to-outside', action='store_true',
-                       help=HelpMessages.PUNCT_TO_OUTSIDE)
-    group.add_argument('--sentence-case', action='store_true',
-                       help=HelpMessages.SENTENCE_CASE)
-    group.add_argument('--single-spaces', action='store_true',
-                       help=HelpMessages.SINGLE_SPACES)
-    group.add_argument('--snake-case', action='store_true',
-                       help=HelpMessages.SNAKE_CASE)
-    group.add_argument('--straight-quotes', action='store_true',
-                       help=HelpMessages.STRAIGHT_QUOTES)
-    group.add_argument('--strikethrough', action='store_true',
-                       help=HelpMessages.STRIKETHROUGH)
-    group.add_argument('--strip', action='store_true',
-                       help=HelpMessages.STRIP)
-    group.add_argument('--title-case', action='store_true',
-                       help=HelpMessages.TITLE_CASE)
-    group.add_argument('--uppercase', action='store_true',
-                       help=HelpMessages.UPPERCASE)
+
+    for arg_key, (_, help_message) in ARGS_MAP.items():
+        group.add_argument(
+            f'--{arg_key}',
+            action='store_true',
+            help=help_message
+        )
 
     # If the user enters the command name with no arguments, print the
     # help messages and exit.
@@ -111,36 +50,8 @@ def parse_args() -> Callable[[str], str]:
 
     args: argparse.Namespace = parser.parse_args()
 
-    # Dictionary mapping argument names to text warping functions
-    arg_func_map: dict[str, Callable[[str], str]] = {
-        'alternating_caps': to_alternating_caps,
-        'binary': to_binary,
-        'camel_case': to_camel_case,
-        'capitalize': capitalize,
-        'cardinal': ordinal_to_cardinal,
-        'curly_quotes': straight_to_curly,
-        'expand_contractions': expand_contractions,
-        'hexadecimal': to_hexadecimal,
-        'hyphens_to_em': hyphens_to_em,
-        'hyphen_to_en': hyphen_to_en,
-        'kebab_case': to_kebab_case,
-        'lowercase': to_lowercase,
-        'ordinal': cardinal_to_ordinal,
-        'pascal_case': to_pascal_case,
-        'punct_to_inside': punct_to_inside,
-        'punct_to_outside': punct_to_outside,
-        'sentence_case': to_sentence_case,
-        'single_spaces': to_single_spaces,
-        'snake_case': to_snake_case,
-        'straight_quotes': curly_to_straight,
-        'strikethrough': strikethrough,
-        'strip': lambda text: text.strip(),
-        'title_case': to_title_case,
-        'uppercase': to_uppercase
-    }
-
-    for arg_label, func in arg_func_map.items():
-        if getattr(args, arg_label):
+    for arg_key, (func, _) in ARGS_MAP.items():
+        if getattr(args, arg_key.replace('-', '_')):
             return func
 
 
