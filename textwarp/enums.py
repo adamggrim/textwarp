0d8f545b3876ab376bc74dba1ecd8e5886773ef0
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-27 10:46:26 -0400

    Create Casing class

--- textwarp/enums.py
+++ textwarp/enums.py
@@ -1,10 +1,22 @@
-from enum import Enum
+from enum import Enum, auto
 
 
+class Casing(Enum):
     """
+    Specify which casing style to apply to a string.
 
     Attributes:
+        TITLE: Represents title case (e.g., A Tale of Two Cities).
+        SENTENCE: Represents sentence case (e.g., A tale of two
+            cities.).
+        CAPITALIZE: Represents capitalizing every word (e.g., A Tale Of
+            Two Cities).
     """
+    TITLE = auto()
+    SENTENCE = auto()
+    CAPITALIZE = auto()
+
+
 class Separator(Enum):
     """
     Specify which separator character to use in a string.
