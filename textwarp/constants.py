e50c06a3a0045fd9e64639b1e7277861522bbd56
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-09-02 09:36:21 -0400

    Create constants for expand_contractions function

--- textwarp/constants.py
+++ textwarp/constants.py
@@ -1,6 +1,15 @@
 # Prompt for the user to enter any other text.
 ANY_OTHER_TEXT_PROMPT: str = 'Any other text? (y/n) (Copy text to clipboard):'
 
+# Variants of 'd for contractions.
+APOSTROPHE_D_VARIANTS: set[str] = {"'d", '’d', '‘d'}
+
+# Variants of 's for contractions.
+APOSTROPHE_S_VARIANTS: set[str] = {"'s", '’s', '‘s'}
+
+# Variants of apostrophes.
+APOSTROPHE_VARIANTS = {"'", '’', '‘'}
+
 # Prompt for when the previous response was invalid.
 ENTER_VALID_RESPONSE_PROMPT: str = 'Please enter a valid response.'
 
@@ -20,6 +29,9 @@ MODIFIED_TEXT_COPIED: str = 'Modified text copied to clipboard.'
 
 # Inputs for indicating a negative response.
 NO_INPUTS: str = {'no', 'n'}
+
+# Part-of-speech tag for past participles.
+PAST_PARTICIPLE_TAGS: set[str] = {'VBN', 'VBD'}
 
 # Inputs for indicating an affirmative response.
 YES_INPUTS: str = {'yes', 'y'}
