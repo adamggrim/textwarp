00bd5934f5925f56f316057fcbbe7e2f41d8e3ef
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-09-11 23:18:45 -0400

    Rename SPLIT_FOR_PASCAL

--- textwarp/regexes.py
+++ textwarp/regexes.py
@@ -145,7 +145,7 @@ class SeparatorCasePatterns:
                                 # character
         ''', re.VERBOSE
     )
-    SPLIT_FOR_PASCAL: re.Pattern[str] = re.compile(rf'''
+    SPLIT_FOR_PASCAL_CASE: re.Pattern[str] = re.compile(rf'''
         # PART 1: SPACE NOT PRECEDED OR FOLLOWED BY A SPACE, PUNCTUATION
         # OR NON-PASCAL CASES
         (?<!                            # Not preceded by...
