d132af64e3e40a49d2057d7702a8125ce4bfff4f
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-10-06 20:24:05 -0400

    Split _helpers.py into _punctuation.py and _casing.py

---
+++ textwarp/_helpers/_punctuation.py
@@ -0,0 +1,36 @@
+import regex as re
+
+from textwarp.regexes import WarpingPatterns
+
+
+def _remove_apostrophes(text: str) -> str:
+    """
+    Remove apostrophes from a string without removing single quotes.
+
+    Args:
+        text: The string to convert.
+
+    Returns:
+        str: The converted string.
+    """
+    return WarpingPatterns.APOSTROPHE_IN_WORD.sub('', text)
+
+
+def _replace_opening_quote(match: re.Match[str]) -> str:
+    """
+    Convert a sequence of straight quotes to opening curly quotes in a
+    given match.
+
+    Args:
+        match: A match object where the first captured group is a
+            string of one or more consecutive straight quote
+            characters.
+
+    Returns:
+        str: A string of opening curly quotes.
+    """
+    quote_chars: str | None = match.group(1) or match.group(2)
+    if quote_chars.startswith("'"):
+        return '‘' * len(quote_chars)
+    else:
+        return '“' * len(quote_chars)
