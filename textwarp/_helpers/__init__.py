e08cc29e84fb792d0219b4a31b174e5a96282805
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-10-07 14:50:30 -0400

    Fix _punctuation import in _helpers __init__

--- textwarp/_helpers/__init__.py
+++ textwarp/_helpers/__init__.py
@@ -4,7 +4,7 @@ from ._case_conversion import (
 )
 from ._entity_capitalization import _to_case_from_doc
 from ._punctuation import (
-    _change_first_letter_case,
-    _remove_apostrophes
+    _remove_apostrophes,
+    _replace_opening_quote
 )
 from ._string_capitalization import _capitalize_from_string
