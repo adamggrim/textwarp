dc3e0e65d2e07511fcc64f58c5558a022ea364fe
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-10-10 12:35:01 -0400

    Create decorators.py

---
+++ textwarp/decorators.py
@@ -0,0 +1,19 @@
+def non_instantiable(cls) -> None:
+    """Class decorator to make a class non-instantiable.
+
+    Args:
+        cls (type): The class to be made non-instantiable.
+
+    Returns:
+        type: The modified class with an __init__ method that always
+            raises a RuntimeError.
+
+    Raises:
+        RuntimeError: When an attempt is made to instantiate the
+            decorated class.
+    """
+    def __init__(self, *args, **kwargs):
+        raise RuntimeError(f'{cls.__name__} cannot be instantiated.')
+
+    cls.__init__ = __init__
+    return cls
