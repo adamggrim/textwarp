47cba61c5d69352a965f54fa3fddad5b3b47a68e
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-10-11 10:03:25 -0400

    Update _helpers/__init__.py

--- textwarp/_helpers/__init__.py
+++ textwarp/_helpers/__init__.py
@@ -1,10 +1,10 @@
 from ._case_conversion import (
-    _change_first_letter_case,
-    _doc_to_case,
-    _to_separator_case
+    change_first_letter_case,
+    doc_to_case,
+    to_separator_case
 )
 from ._punctuation import (
-    _remove_apostrophes,
-    _replace_opening_quote
+    remove_apostrophes,
+    replace_opening_quote
 )
 from ._string_capitalization import capitalize_from_string
