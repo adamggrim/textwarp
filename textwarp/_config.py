82a6a6ad290ae0768101fc95a4a05220c48b7e91
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-10-28 03:11:38 -0400

    Update config.py to use pathlib

--- textwarp/_config.py
+++ textwarp/_config.py
@@ -1,112 +1,116 @@
 import json
-import os
 from typing import Final, Union, cast
-
-
-# Get the directory of the current file.
-file_dir: str = os.path.dirname(__file__)
+from pathlib import Path
 
 # Define a type for JSON data.
 JSONType = Union[dict, list, str, int, float, bool, None]
 
+# Path to the base directory for data files.
+data_root: Path = Path(__file__).parent / '_data'
 
-def _load_json_from_data(filename: str) -> JSONType:
+
+def _load_json_from_data(relative_path: Path) -> JSONType:
     """
-    Construct a path to a file in the ``data`` subdirectory and load it
-    as a dictionary.
-
-    This function can also load files from subdirectories within the
-    ``data`` folder.
+    Construct the path to a file relative to the base data directory and
+    load its contents as a JSON object.
 
     Args:
-        filename: The name of the JSON file.
+        relative_path: The name of the JSON file.
 
     Returns:
         JSONType: The JSON file loaded as a Python object.
     """
     # Construct a platform-independent path to the JSON file.
-    json_file_path: str = os.path.join(file_dir, '_data', filename)
+    json_file_path: Path = data_root / relative_path
 
     # Load and return the JSON data.
     with open(json_file_path, 'r') as json_file:
-        return json.load(json_file)
+        return cast(JSONType, json.load(json_file))
 
 
-# Abbreviations that are always capitalized.
+# Capitalization mappings for entities with multiple possible
+# capitalizations.
 AMBIGUOUS_CAPITALIZATIONS: Final[dict[str, str]] = cast(
-    dict[str, str], _load_json_from_data('ambiguous_capitalizations_map.json')
-)
-
-
-# Contractions that can expand to multiple phrases.
-AMBIGUOUS_CONTRACTIONS: Final[list[str]] = cast(
-    list[str], _load_json_from_data('ambiguous_contractions.json')
+    dict[str, str], _load_json_from_data(
+        Path('_entity_capitalization') / 'ambiguous_capitalizations_map.json'
+    )
 )
 
 # Abbreviations that are always capitalized.
 CAPITALIZED_ABBREVIATIONS_MAP: Final[dict[str, str]] = cast(
-    dict[str, str], _load_json_from_data('capitalized_abbreviations_map.json')
+    dict[str, str], _load_json_from_data(Path(
+        'capitalized_abbreviations_map.json'
+    ))
 )
 
 # Suffix tokens derived from contractions.
 CONTRACTION_SUFFIX_TOKENS: Final[list[str]] = cast(
-    list[str], _load_json_from_data('contraction_suffix_tokens.json')
+    list[str], _load_json_from_data(Path('contraction_suffix_tokens.json'))
 )
 
 # Map pairing each contraction with its expanded version.
 CONTRACTIONS_MAP: Final[dict[str, str]] = cast(
-    dict[str, str], _load_json_from_data('contractions_map.json')
+    dict[str, str], _load_json_from_data(
+        Path('_contraction_expansion') / 'contractions_map.json'
+    )
+)
+
+# Map pairing each custom entity with its capitalized version.
+CUSTOM_ENTITIES_MAP: Final[dict[str, str]] = cast(
+    dict[str, str], _load_json_from_data(
+        Path('_entity_capitalization') / 'custom_entities_map.json'
+    )
 )
 
 # Words that are elided for certain contractions.
 ELISION_WORDS: Final[set[str]] = set(
-    cast(list[str], _load_json_from_data('elision_words.json'))
+    cast(list[str], _load_json_from_data(Path('elision_words.json')))
 )
 
 # Map pairing each initialism with its capitalized version.
 INITIALISMS_MAP: Final[dict[str, str]] = cast(
-    dict[str, str], _load_json_from_data('initialisms_map.json')
+    dict[str, str], _load_json_from_data(Path('initialisms_map.json'))
 )
 
 # Abbreviations that are always lowercase.
 LOWERCASE_ABBREVIATIONS: Final[set[str]] = set(
-    cast(list[str], _load_json_from_data('lowercase_abbreviations.json'))
+    cast(list[str], _load_json_from_data(Path('lowercase_abbreviations.json')))
 )
 
 # Lowercase particles that are not capitalized in title case.
 LOWERCASE_PARTICLES: Final[set[str]] = set(
-    cast(list[str], _load_json_from_data('lowercase_particles.json'))
+    cast(list[str], _load_json_from_data(Path('lowercase_particles.json')))
 )
 
 # Suffixes to split off from map-capitalized words.
 MAP_SUFFIX_EXCEPTIONS: Final[list[str]] = cast(
-    list[str], _load_json_from_data('map_suffix_exceptions.json')
+    list[str], _load_json_from_data(Path('map_suffix_exceptions.json'))
 )
 
 # Map pairing each mixed-case word with its lowercase version.
 MIXED_CASE_WORDS_MAP: Final[dict[str, str]] = cast(
-    dict[str, str], _load_json_from_data('mixed_case_words_map.json')
+    dict[str, str], _load_json_from_data(Path('mixed_case_words_map.json'))
 )
 
 # Map pairing each character with its Morse code equivalent.
 MORSE_MAP: Final[dict[str, str]] = cast(
-    dict[str, str], _load_json_from_data('morse_map.json')
+    dict[str, str], _load_json_from_data(Path('morse_map.json'))
 )
 
 # Name prefixes that necessitate special name casing.
 NAME_PREFIXES: Final[list[str]] = cast(
-    list[str], _load_json_from_data('name_prefixes.json')
+    list[str], _load_json_from_data(Path('name_prefixes.json'))
 )
 
 # Words with name prefixes that do not follow standard prefix rules.
 NAME_PREFIX_EXCEPTIONS: Final[list[str]] = cast(
-    list[str], _load_json_from_data('name_prefix_exceptions.json')
+    list[str], _load_json_from_data(Path('name_prefix_exceptions.json'))
 )
 
 # Map pairing each prefixed name not following standard prefix rules
 # with its capitalized version.
 OTHER_PREFIXED_NAMES_MAP: Final[dict[str, str]] = cast(
-    dict[str, str], _load_json_from_data('other_prefixed_names_map.json')
+    dict[str, str], _load_json_from_data(Path('other_prefixed_names_map.json'))
 )
 
 # Map for decoding Morse code.
