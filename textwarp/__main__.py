c653fb8988e83cb7f7f9986b597133d4e6d1b54b
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-11-06 01:30:12 -0500

    Add ability to use warping or analysis commands

--- textwarp/__main__.py
+++ textwarp/__main__.py
@@ -1,17 +1,31 @@
-from textwarp._parsing import parse_args
-from textwarp._ui import (
-    warp_text,
+import textwarp._analysis_commands as analysis_mod
+import textwarp.warping as warping_mod
+
+from ._parsing import parse_args
+from ._runners import (
+    analyze_text,
+    warp_text
+)
+from ._ui import (
     print_padding,
     program_exit
 )
 
+WARPING_COMMANDS: set[str] = set(warping_mod.__all__)
+ANALYSIS_COMMANDS: set[str] = set(analysis_mod.__all__)
+
 
 def main() -> None:
     """Run the main loop for text warping."""
-    warping_function: str = parse_args()
+    command_name: str = parse_args()
 
     try:
-        warp_text(warping_function)
+        if command_name in WARPING_COMMANDS:
+            warp_text(command_name)
+        elif command_name in ANALYSIS_COMMANDS:
+            analyze_text(command_name)
+        else:
+            print(f"Command '{command_name}' not recognized.")
         program_exit()
     except KeyboardInterrupt:
         print_padding()
