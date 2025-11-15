b6d7ca9bd2af2354154b7407e25effab9a6e8314
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-11-14 23:14:03 -0500

    Update main function with command_name, func_name as parse_args return

--- textwarp/__main__.py
+++ textwarp/__main__.py
@@ -33,18 +33,23 @@ WARPING_COMMANDS: set[str] = (
 
 def main() -> None:
     """Run the main loop for text transformation or analysis."""
-    command_name: str = parse_args()
+    command_name: str # Hyphenated command name
+    func_name: str # Underscored function name
+    command_name, func_name = parse_args()
 
     try:
-        if command_name in ANALYSIS_COMMANDS:
-            analyze_text(command_name)
-        elif command_name in REPLACEMENT_COMMANDS:
-            replace_text(command_name)
-        elif command_name == 'clear':
+        if func_name in ANALYSIS_FUNC_NAMES:
+            analyze_text(func_name)
+        elif func_name in REPLACEMENT_FUNC_NAMES:
+            replace_text(func_name)
+        elif func_name in CLEAR_FUNC_NAMES:
             clear_clipboard()
-        elif command_name in ALL_COMMANDS:
-            warp_text(command_name)
+        # Any function that is not for analysis, replacement or clearing
+        # is for warping.
+        elif func_name in ALL_FUNC_NAMES:
+            warp_text(func_name)
         else:
+            # Print the user-facing command name in the error.
             print(f"\nCommand '{command_name}' not recognized.")
         program_exit()
     except KeyboardInterrupt:
