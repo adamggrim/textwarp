abd62e0c2353cf0e7d5ab5a24d93ac48a422fc8b
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-11-15 20:56:10 -0500

    Create _apostrophes.py

---
+++ textwarp/_helpers/_apostrophes.py
@@ -0,0 +1,14 @@
+from .._regexes import WarpingPatterns
+
+
+def remove_apostrophes(text: str) -> str:
+    """
+    Remove apostrophes from a string without removing single quotes.
+
+    Args:
+        text: The string to convert.
+
+    Returns:
+        str: The converted string.
+    """
+    return WarpingPatterns.APOSTROPHE_IN_WORD.sub('', text)
