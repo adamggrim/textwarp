783dd505c46ac8112ba478b6ab48f8f9fb21fd36
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-11-22 11:31:58 -0500

    Create expand_ambiguous_contraction function

--- textwarp/_helpers/_contraction_expansion.py
+++ textwarp/_helpers/_contraction_expansion.py
@@ -32,6 +32,88 @@ def apply_expansion_casing(original_word: str, expanded_word: str) -> str:
 
 
 def repl_contraction_from_dict(
+def expand_ambiguous_contraction(
+    contraction: str,
+    suffix_token: Token,
+    doc: Doc,
+) -> str:
+    """
+    Helper function to replace a matched ambiguous contraction with its
+    expanded version.
+
+    This function uses spaCy to disambiguate contractions based on
+    context.
+
+    Args:
+        contraction: The contraction to expand.
+        suffix_token: The token containing the contraction suffix
+            (e.g., "'s", "n't").
+        doc: The spaCy ``Doc`` containing the contraction.
+
+    Returns:
+        str: The expanded version of the matched contraction.
+    """
+    full_expansion: str = contraction
+
+    if suffix_token and suffix_token.i > 0:
+        previous_token: Token = doc[suffix_token.i - 1]
+        next_token: Token | None = (
+            doc[suffix_token.i + 1]
+            if suffix_token.i < len(doc) - 1
+            else None
+        )
+        # Disambiguate ain't: "am not," "is not," "are not," "has
+        # not" or "have not"
+        if (previous_token.lower_ == 'ai' and
+                suffix_token.lower_ in AIN_T_SUFFIX_VARIANTS):
+            if suffix_token.i >= 2:
+                subject_token: Token = doc[suffix_token.i - 2]
+                # Default expansion
+                full_expansion = 'am not'
+                # 1. Check for "has"/"have not".
+                if next_token and next_token.tag_ in PAST_PARTICIPLE_TAGS:
+                    if subject_token.lower_ in ('he', 'she', 'it'):
+                        full_expansion = 'has not'
+                    else:
+                        # Covers "I", "you", "we" and "they".
+                        full_expansion = 'have not'
+                # 2. Check for "is"/"am"/"are not".
+                else:
+                    if subject_token.lower_ == 'i':
+                        full_expansion = 'am not'
+                    elif subject_token.lower_ in ('he', 'she', 'it'):
+                        full_expansion = 'is not'
+                    else:
+                        # Covers "you", "we" and "they".
+                        full_expansion = 'are not'
+            else:
+                full_expansion = 'am not'
+
+            return apply_expansion_casing(contraction, full_expansion)
+
+        else:
+            expanded_suffix = ''
+            # Disambiguate 's: "is" vs. "has"
+            if suffix_token.lower_ in APOSTROPHE_S_VARIANTS:
+                if next_token and next_token.tag_ in PAST_PARTICIPLE_TAGS:
+                    expanded_suffix = 'has'
+                else:
+                    expanded_suffix = 'is'
+            # Disambiguate 'd: "would" vs. "had"
+            elif suffix_token.lower_ in APOSTROPHE_D_VARIANTS:
+                if next_token and next_token.tag_ in PAST_PARTICIPLE_TAGS:
+                    expanded_suffix = 'had'
+                else:
+                    expanded_suffix = 'would'
+
+            if expanded_suffix:
+                subject_token: Token = doc[suffix_token.i - 1]
+                # Combine the subject with the expanded suffix.
+                full_expansion: str = f'{subject_token.text} {expanded_suffix}'
+
+        return apply_expansion_casing(contraction, full_expansion)
+
+
     contraction: str,
     contractions_map: dict[str, str]
 ) -> str:
