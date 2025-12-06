15828a688fb59b43c622b26ea60c119afff089bf
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-12-05 21:34:07 -0500

    Update _helpers init

--- textwarp/_helpers/__init__.py
+++ textwarp/_helpers/__init__.py
@@ -1,12 +1,12 @@
 from ._case_conversion import (
     change_first_letter_case,
     doc_to_case,
-    to_separator_case
+    to_separator_case,
+    word_to_pascal
 )
 from ._contraction_expansion import expand_contractions_from_doc
-from ._apostrophes import (
-    remove_apostrophes
-)
+from ._apostrophes import remove_apostrophes
+from ._nlp_processing import process_as_doc
 from ._quote_conversion import (
     curly_to_straight,
     straight_to_curly
