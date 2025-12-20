e3e9ff229fd612720853b33eb58f49237f9a1d1f
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-12-20 11:19:24 -0500

    Create CASE_NAMES_MAP

--- textwarp/_validation.py
+++ textwarp/_validation.py
@@ -1,9 +1,35 @@
 
+from ._constants import CASE_NAMES_MAP
 from ._exceptions import (
     EmptyClipboardError,
     WhitespaceClipboardError,
 )
+from .warping import (
+    to_camel_case,
+    to_dot_case,
+    to_kebab_case,
+    to_pascal_case,
+    to_snake_case
+)
 
+
+# Mapping of valid case name inputs to their standardized names.
+CASE_NAMES_MAP: Final[dict[str, Callable[[str], str]]]= {
+    'camel': to_camel_case,
+    'camel case': to_camel_case,
+    'dot': to_dot_case,
+    'dot case': to_dot_case,
+    'lower': str.lower,
+    'lowercase': str.lower,
+    'kebab': to_kebab_case,
+    'kebab case': to_kebab_case,
+    'pascal': to_pascal_case,
+    'pascal case': to_pascal_case,
+    'snake': to_snake_case,
+    'snake case': to_snake_case,
+    'upper': str.upper,
+    'uppercase': str.upper,
+}
 
 
 def validate_clipboard(clipboard: str) -> None:
