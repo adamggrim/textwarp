85f8dcb9dfc5556871a34ed820eb4c82950b06ab
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-12-20 01:21:31 -0500

    Create _exceptions.py

---
+++ textwarp/_exceptions.py
@@ -0,0 +1,35 @@
+class CaseNotFoundError(Exception):
+    """Exception raised when the provided case is not found in the
+    searched text."""
+
+
+class InvalidCaseNameError(Exception):
+    """Exception raised when the provided case name string is
+    invalid."""
+
+
+class InvalidRegexError(Exception):
+    """Exception raised when the provided regex string is invalid."""
+
+
+class NoRegexError(Exception):
+    """Exception raised when the provided regex string is empty."""
+
+
+class NoCaseNameError(Exception):
+    """Exception raised when the provided case name string is empty."""
+
+
+class NoReplacementTextError(Exception):
+    """Exception raised when the provided replacement text string is
+    empty."""
+
+
+class RegexNotFoundError(Exception):
+    """Exception raised when the provided regex string is not found in
+    the searched text."""
+
+
+class TextToReplaceNotFoundError(Exception):
+    """Exception raised when the provided text to replace is not found
+    in the searched text."""
