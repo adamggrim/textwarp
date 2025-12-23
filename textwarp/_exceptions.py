97ff3f0c5dda7740e01403aabc5b9856b7f33780
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-12-23 11:23:26 -0500

    Add WhitespaceCaseNameError to _exceptions.py

--- textwarp/_exceptions.py
+++ textwarp/_exceptions.py
@@ -2,8 +2,10 @@ class CaseNotFoundError(Exception):
     """Exception raised when the provided case is not found in the
     searched text."""
 
+
 class EmptyClipboardError(Exception):
     """Exception raised when the clipboard is empty."""
+
 
 class InvalidCaseNameError(Exception):
     """Exception raised when the provided case name string is
@@ -26,6 +28,10 @@ class RegexNotFoundError(Exception):
 class TextToReplaceNotFoundError(Exception):
     """Exception raised when the provided text to replace is not found
     in the searched text."""
+
+
+class WhitespaceCaseNameError(Exception):
+    """Exception raised when the case name contains only whitespace."""
 
 
 class WhitespaceClipboardError(Exception):
