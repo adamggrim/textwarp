798d20fa01507fac127df9a81bc181a67329e131
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-12-27 11:00:26 -0500

    Create CASE_NAMES_REGEX_MAP

--- textwarp/_constants/definitions.py
+++ textwarp/_constants/definitions.py
@@ -45,6 +45,23 @@ CASE_NAMES_FUNC_MAP: Final[dict[str, Callable[[str], str]]]= {
     'uppercase': str.upper,
 }
 
+CASE_NAMES_REGEX_MAP: Final[dict[str, re.Pattern[str]]] = {
+    'camel': CasePatterns.CAMEL_WORD,
+    'camel case': CasePatterns.CAMEL_WORD,
+    'dot': CasePatterns.DOT_WORD,
+    'dot case': CasePatterns.DOT_WORD,
+    'kebab': CasePatterns.KEBAB_WORD,
+    'kebab case': CasePatterns.KEBAB_WORD,
+    'lower': CasePatterns.LOWER_WORD,
+    'lowercase': CasePatterns.LOWER_WORD,
+    'pascal': CasePatterns.PASCAL_WORD,
+    'pascal case': CasePatterns.PASCAL_WORD,
+    'snake': CasePatterns.SNAKE_WORD,
+    'snake case': CasePatterns.SNAKE_WORD,
+    'upper': CasePatterns.UPPER_WORD,
+    'uppercase': CasePatterns.UPPER_WORD,
+}
+
 # Inputs for exiting the program.
 EXIT_INPUTS: Final = {'quit', 'q', 'exit', 'e'}
 
