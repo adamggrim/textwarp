ccde76f7699de8154d51b507c08b5196517878da
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-01-07 01:11:33 -0500

    Add newlines to _config.py

--- textwarp/_config.py
+++ textwarp/_config.py
@@ -44,17 +44,20 @@ def get_absolute_entities_map() -> dict[str, str]:
         ENTITY_CAPITALIZATION_DIR / 'absolute_entities_map.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_ambiguous_contractions() -> list[str]:
     return cast(list[str], _load_json_from_data(
         CONTRACTION_EXPANSION_DIR / 'ambiguous_contractions.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_capitalized_abbreviations_map() -> dict[str, str]:
     return cast(dict[str, str], _load_json_from_data(
         STRING_CAPITALIZATION_DIR / 'capitalized_abbreviations_map.json'
     ))
+
 
 @lru_cache(maxsize=1)
 def get_contextual_entities_map() -> (
@@ -64,15 +67,18 @@ def get_contextual_entities_map() -> (
         ENTITY_CAPITALIZATION_DIR / 'contextual_entities_map.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_contraction_suffixes() -> list[str]:
     return cast(list[str], _load_json_from_data(
         ENTITY_CAPITALIZATION_DIR / 'contraction_suffixes.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_elision_words() -> set[str]:
     return set(cast(list[str], _load_json_from_data('elision_words.json')))
+
 
 @lru_cache(maxsize=1)
 def get_initialisms_map() -> dict[str, str]:
@@ -80,11 +86,13 @@ def get_initialisms_map() -> dict[str, str]:
         STRING_CAPITALIZATION_DIR / 'initialisms_map.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_lowercase_abbreviations() -> set[str]:
     return set(cast(list[str], _load_json_from_data(
         STRING_CAPITALIZATION_DIR / 'lowercase_abbreviations.json'
     )))
+
 
 @lru_cache(maxsize=1)
 def get_lowercase_particles() -> set[str]:
@@ -92,11 +100,13 @@ def get_lowercase_particles() -> set[str]:
         ENTITY_CAPITALIZATION_DIR / 'lowercase_particles.json'
     )))
 
+
 @lru_cache(maxsize=1)
 def get_map_suffix_exceptions() -> list[str]:
     return cast(list[str], _load_json_from_data(
         STRING_CAPITALIZATION_DIR / 'map_suffix_exceptions.json'
     ))
+
 
 @lru_cache(maxsize=1)
 def get_mixed_case_words_map() -> dict[str, str]:
@@ -104,9 +114,11 @@ def get_mixed_case_words_map() -> dict[str, str]:
         STRING_CAPITALIZATION_DIR / 'mixed_case_words_map.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_morse_map() -> dict[str, str]:
     return cast(dict[str, str], _load_json_from_data('morse_map.json'))
+
 
 @lru_cache(maxsize=1)
 def get_name_prefix_exceptions() -> list[str]:
@@ -114,11 +126,13 @@ def get_name_prefix_exceptions() -> list[str]:
         STRING_CAPITALIZATION_DIR / 'name_prefix_exceptions.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_name_prefixes() -> list[str]:
     return cast(list[str], _load_json_from_data(
         STRING_CAPITALIZATION_DIR / 'name_prefixes.json'
     ))
+
 
 @lru_cache(maxsize=1)
 def get_other_prefixed_names_map() -> dict[str, str]:
@@ -126,9 +140,11 @@ def get_other_prefixed_names_map() -> dict[str, str]:
         STRING_CAPITALIZATION_DIR / 'other_prefixed_names_map.json'
     ))
 
+
 @lru_cache(maxsize=1)
 def get_reversed_morse_map() -> dict[str, str]:
     return {value: key for key, value in get_morse_map().items()}
+
 
 @lru_cache(maxsize=1)
 def get_unambiguous_contractions_map() -> dict[str, str]:
