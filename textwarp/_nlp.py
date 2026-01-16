c31daf228d2cd74734cffa35c8f3063c7978eb85
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-01-15 23:59:27 -0500

    Create _nlp.py

--- textwarp/_nlp.py
+++ textwarp/_nlp.py
@@ -1,29 +1,43 @@
 """Function for lazy loading of the spaCy model."""
 
-from typing import Optional
+from typing import (
+    Literal,
+    Optional
+)
 
 import spacy
 
 from ._ui import print_wrapped
 
-_nlp_instance: Optional[spacy.language.Language] = None
+ModelSize = Literal['small', 'large']
+_nlp_instances: dict[str, spacy.language.Language] = {}
 
-def get_nlp():
+
+def get_nlp(size: ModelSize = 'small'):
     """
     Returns the loaded spaCy model instance, downloading it if not
     found.
 
+    Args:
+        size: The size of the spaCy model to load. Can be either
+            "small" (speed) or "large" (accuracy).
+
     Returns:
         spacy.language.Language: The loaded spaCy model instance.
     """
-    global _nlp_instance
-    if _nlp_instance is None:
-        model_name = 'en_core_web_trf'
-        try:
-            _nlp_instance = spacy.load(model_name)
-        except OSError:
-            print_wrapped(f"Downloading spaCy model '{model_name}'...")
-            spacy.cli.download(model_name)
-            _nlp_instance = spacy.load(model_name)
+    model_map = {
+        'small': 'en_core_web_sm',
+        'large': 'en_core_web_trf'
+    }
 
-    return _nlp_instance
+    target_model = model_map.get(size, 'en_core_web_sm')
+
+    if target_model not in _nlp_instances:
+        try:
+            _nlp_instances[target_model] = spacy.load(target_model)
+        except OSError:
+            print_wrapped(f"Downloading spaCy model '{target_model}'...")
+            spacy.cli.download(target_model)
+            _nlp_instances[target_model] = spacy.load(target_model)
+
+    return _nlp_instances[target_model]
