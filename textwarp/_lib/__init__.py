1c224e08724168fc99c77458a155b3adf812b39e
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-01-29 16:01:15 -0500

    Create init files for _lib and _lib/casing folders

---
+++ textwarp/_lib/__init__.py
@@ -0,0 +1,9 @@
+"""Exposes library functions for use across the package."""
+
+from .casing import *
+from .contractions import *
+from .encoding import *
+from .manipulation import *
+from .nlp import *
+from .numbers import *
+from .punctuation import *
---
+++ textwarp/_lib/casing/__init__.py
@@ -0,0 +1,6 @@
+"""Exposes casing logic for use across the package."""
+
+from .case_conversion import *
+from .entity_casing import *
+from .string_casing import *
+from .token_casing import *
