a2bd635cd894f8d2af03aeff31be26e78fbdb6fb
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-02-26 01:33:20 -0500

    Remove duplicate functions

--- textwarp/_lib/contractions/handlers.py
+++ textwarp/_lib/contractions/handlers.py
@@ -121,52 +121,6 @@ def handle_gotta(span: Span) -> tuple[str, int] | None:
     return cased_text, span.end_char
 
 
-def handle_wanna(span: Span) -> tuple[str, int] | None:
-    """
-    Replace a matched "wanna" contraction with its expanded version.
-
-    Args:
-        span: The spaCy ``Span`` containing the contraction.
-
-    Returns:
-        tuple[str, int] | None: A tuple containing:
-            1. The expanded version of the matched contraction.
-            2. The end index of the expanded contraction; otherwise
-                ``None``.
-    """
-    if span.text.lower() != 'wanna':
-        return None
-
-    base_verb = disambiguate_wanna(span)
-
-    expanded_text: str = f'want {base_verb}'
-    cased_text: str = apply_expansion_casing(span.text, expanded_text)
-    return cased_text, span.end_char
-
-
-def handle_whatcha(span: Span) -> tuple[str, int] | None:
-    """
-    Replace a matched "whatcha" contraction with its expanded version.
-
-    Args:
-        span: The spaCy ``Span`` containing the contraction.
-
-    Returns:
-        tuple[str, int] | None: A tuple containing:
-            1. The expanded version of the matched contraction.
-            2. The end index of the expanded contraction; otherwise
-                ``None``.
-    """
-    if span.text.lower() != 'whatcha':
-        return None
-
-    base_verb: str = disambiguate_whatcha(span)
-    expanded_text: str = f'what {base_verb} you'
-    cased_text: str = apply_expansion_casing(span.text, expanded_text)
-
-    return cased_text, span.end_char
-
-
 def handle_negation(span: Span) -> tuple[str, int] | None:
     """
     Replace a matched negative contraction (including "ain't") with its
