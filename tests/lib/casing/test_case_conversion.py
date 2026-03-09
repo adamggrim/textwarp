1bbc58bbd094da488a426224480a88ffa4adc795
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-03-09 05:17:12 -0400

    Create test_case_conversion.py

---
+++ tests/lib/casing/test_case_conversion.py
@@ -0,0 +1,98 @@
+"""Tests for converting between cases."""
+
+from textwarp._lib.casing.case_conversion import (
+    change_first_letter_case,
+    doc_to_case,
+    find_first_alphabetical_idx,
+    to_separator_case,
+    word_to_pascal
+)
+from textwarp._core.enums import CaseSeparator, Casing
+from textwarp._lib.nlp import process_as_doc
+
+
+def test_find_first_alphabetical_idx():
+    """
+    Test finding the first letter in strings with non-alphabetical
+    characters.
+    """
+    assert find_first_alphabetical_idx('10things') == 2
+    assert find_first_alphabetical_idx('***') is None
+    assert find_first_alphabetical_idx('Taming') == 0
+
+
+def test_change_first_letter_case():
+    """Test changing only the first alphabetical character's case."""
+    assert change_first_letter_case('100 indecisions', str.upper) == (
+        '100 Indecisions'
+    )
+    assert change_first_letter_case('bang', str.upper) == 'Bang'
+    assert change_first_letter_case('WHIMPER', str.lower) == 'whimper'
+    assert change_first_letter_case('!!!', str.upper) == '!!!'
+
+
+def test_word_to_pascal():
+    """Test converting a single word to PascalCase."""
+    assert word_to_pascal('451') == '451'
+    assert word_to_pascal('PascalCase') == 'PascalCase'
+    assert word_to_pascal('camelCase') == 'CamelCase'
+    assert word_to_pascal('case') == 'Case'
+
+
+def test_to_separator_case_basic():
+    """Test converting standard spaced text to separator cases."""
+    assert to_separator_case('bell jar', CaseSeparator.SNAKE) == 'bell_jar'
+    assert to_separator_case('bell jar', CaseSeparator.KEBAB) == 'bell-jar'
+    assert to_separator_case('bell jar', CaseSeparator.DOT) == 'bell.jar'
+
+
+def test_to_separator_case_existing_cases():
+    """
+    Test converting from existing programming cases to a new separator case.
+    """
+    assert to_separator_case(
+        'existence-precedes-essence', CaseSeparator.SNAKE
+    ) == 'existence_precedes_essence'
+    assert to_separator_case('being.nothingness', CaseSeparator.KEBAB) == 'being-nothingness'
+    assert to_separator_case('sisyphusMyth', CaseSeparator.SNAKE) == 'sisyphus_myth'
+    assert to_separator_case('SørenKierkegaard', CaseSeparator.DOT) == 'søren.kierkegaard'
+
+
+def test_to_separator_case_non_alpha():
+    """Test that separator casing safely handles numbers and symbols."""
+    assert to_separator_case('April 4th 1984', CaseSeparator.SNAKE) == 'april_4th_1984'
+
+
+def test_doc_to_case_sentence():
+    """Test applying sentence case to a spaCy Doc."""
+    doc = process_as_doc('I HAVE NO MOUTH, AND I MUST SCREAM.')
+    result = doc_to_case(doc, Casing.SENTENCE)
+    assert result == 'I have no mouth, and i must scream.'
+
+    doc_mixed = process_as_doc(
+        'start writing. short sentences. describe it. just describe it.'
+    )
+    result_mixed = doc_to_case(doc_mixed, Casing.SENTENCE)
+    assert result_mixed == (
+        'Start writing. Short sentences. Describe it. Just describe it.'
+    )
+
+
+def test_doc_to_case_start():
+    """Test applying start case to a spaCy Doc."""
+    doc = process_as_doc('not that innocent')
+    result = doc_to_case(doc, Casing.START)
+    assert result == 'Not That Innocent'
+
+
+def test_doc_to_case_title():
+    """Test applying title case to a spaCy Doc."""
+    doc = process_as_doc(
+        'the tragical history of the life and death of doctor faustus'
+    )
+    result = doc_to_case(doc, Casing.TITLE)
+
+    assert 'The Tragical History' in result
+    assert 'of the Life' in result
+    assert 'and Death' in result
+    assert 'of Doctor Faustus' in result
