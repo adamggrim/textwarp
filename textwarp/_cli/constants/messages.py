0e869fa77231ed130d17f2420982819cfa8096bb
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-03-17 22:57:09 -0400

    Update messages.py to use translatable strings

--- textwarp/_cli/constants/messages.py
+++ textwarp/_cli/constants/messages.py
@@ -1,6 +1,9 @@
 """Strings for displaying command-line messages."""
 
+import gettext
 from typing import Final
+
+_ = gettext.gettext
 
 __all__ = [
     'ANY_OTHER_TEXT_PROMPT',
@@ -27,68 +30,68 @@ __all__ = [
 ]
 
 # Prompt for the user to enter any other text.
-ANY_OTHER_TEXT_PROMPT: Final = (
+ANY_OTHER_TEXT_PROMPT: Final = _(
     'Any other text? (y/n) (Copy text to clipboard):'
 )
 
 # Message for when the case is not found in the text.
-CASE_NOT_FOUND_MESSAGE: Final = 'Case not found.'
+CASE_NOT_FOUND_MESSAGE: Final = _('Case not found.')
 
 # Message for any error accessing the clipboard.
-CLIPBOARD_ACCESS_ERROR_MESSAGE: Final = 'Error accessing clipboard: '
+CLIPBOARD_ACCESS_ERROR_MESSAGE: Final = _('Error accessing clipboard: ')
 
 # Message for when the clipboard is cleared.
-CLIPBOARD_CLEARED_MESSAGE: Final = 'Clipboard text cleared.'
+CLIPBOARD_CLEARED_MESSAGE: Final = _('Clipboard text cleared.')
 
 # Prompt for the user to enter a case to replace.
-ENTER_CASE_TO_REPLACE_PROMPT: Final = 'Enter a case to replace:'
+ENTER_CASE_TO_REPLACE_PROMPT: Final = _('Enter a case to replace:')
 
 # Prompt for the user to enter the number of most frequent words.
-ENTER_MFW_COUNT_PROMPT: Final = 'How many most frequent words?'
+ENTER_MFW_COUNT_PROMPT: Final = _('How many most frequent words?')
 
 # Prompt for the user to enter a regular expression to find.
-ENTER_REGEX_PROMPT: Final = 'Enter a regular expression to replace:'
+ENTER_REGEX_PROMPT: Final = _('Enter a regular expression to replace:')
 
 # Prompt for the user to enter a replacement case.
-ENTER_REPLACEMENT_CASE_PROMPT: Final = 'Enter a replacement case:'
+ENTER_REPLACEMENT_CASE_PROMPT: Final = _('Enter a replacement case:')
 
 # Prompt for the user to enter replacement text.
-ENTER_REPLACEMENT_TEXT_PROMPT: Final = 'Enter replacement text:'
+ENTER_REPLACEMENT_TEXT_PROMPT: Final = _('Enter replacement text:')
 
 # Prompt for the user to enter text to find.
-ENTER_TEXT_TO_REPLACE_PROMPT: Final = 'Enter text to replace:'
+ENTER_TEXT_TO_REPLACE_PROMPT: Final = _('Enter text to replace:')
 
 # Prompt for when the previous case name was invalid.
-ENTER_VALID_CASE_PROMPT: Final = 'Please enter a valid case.'
+ENTER_VALID_CASE_PROMPT: Final = _('Please enter a valid case.')
 
 # Prompt for when the previous response was not a number.
-ENTER_VALID_NUMBER_PROMPT: Final = 'Please enter a valid number.'
+ENTER_VALID_NUMBER_PROMPT: Final = _('Please enter a valid number.')
 
 # Prompt for when the previous regex was invalid.
-ENTER_VALID_REGEX_PROMPT: Final = 'Please enter a valid regular expression.'
+ENTER_VALID_REGEX_PROMPT: Final = _('Please enter a valid regular expression.')
 
 # Prompt for when the previous response was invalid.
-ENTER_VALID_RESPONSE_PROMPT: Final = 'Please enter a valid response (y/n).'
+ENTER_VALID_RESPONSE_PROMPT: Final = _('Please enter a valid response (y/n).')
 
 # Prompt for when the previous text was invalid.
-ENTER_VALID_TEXT_PROMPT: Final = 'Please enter valid text.'
+ENTER_VALID_TEXT_PROMPT: Final = _('Please enter valid text.')
 
 # Prompt for the user to enter the words per minute.
-ENTER_WPM_PROMPT: Final = 'How many words per minute?'
+ENTER_WPM_PROMPT: Final = _('How many words per minute?')
 
 # Message for when the user exits the program.
-EXIT_MESSAGE: Final = 'Exiting the program...'
+EXIT_MESSAGE: Final = _('Exiting the program...')
 
 # Help description for command-line arguments.
-HELP_DESCRIPTION: Final = (
+HELP_DESCRIPTION: Final = _(
     'Specify which text warping function to apply to the clipboard.'
 )
 
 # Message for when the program copies modified text to the clipboard.
-MODIFIED_TEXT_COPIED_MESSAGE: Final = 'Modified text copied to clipboard.'
+MODIFIED_TEXT_COPIED_MESSAGE: Final = _('Modified text copied to clipboard.')
 
 # Message for when the regular expression is not found in the text.
-REGEX_NOT_FOUND_MESSAGE: Final = 'Regular expression not found.'
+REGEX_NOT_FOUND_MESSAGE: Final = _('Regular expression not found.')
 
 # Message for when the text to replace is not found in the text.
-TEXT_NOT_FOUND_MESSAGE: Final = 'Text not found.'
+TEXT_NOT_FOUND_MESSAGE: Final = _('Text not found.')
