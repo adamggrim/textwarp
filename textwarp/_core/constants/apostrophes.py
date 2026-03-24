c4e9fbb3af1ac2614dad99849793822f47c44c6e
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-03-24 01:36:09 -0400

    Delete AIN_T_SUFFIX_VARIANTS, APOSTROPHE_D_VARIANTS and APOSTROPHE_S_VARIANTS from apostrophes.py

--- textwarp/_core/constants/apostrophes.py
+++ textwarp/_core/constants/apostrophes.py
@@ -2,21 +2,7 @@
 
 from typing import Final
 
-__all__ = [
-    'AIN_T_SUFFIX_VARIANTS',
-    'APOSTROPHE_D_VARIANTS',
-    'APOSTROPHE_S_VARIANTS',
-    'OPEN_QUOTES'
-]
-
-# Variants of 's for contractions.
-AIN_T_SUFFIX_VARIANTS: Final[frozenset[str]] = frozenset({"n't", 'n’t', 'n‘t'})
-
-# Variants of 'd for contractions.
-APOSTROPHE_D_VARIANTS: Final[frozenset[str]] = frozenset({"'d", '’d', '‘d'})
-
-# Variants of 's for contractions.
-APOSTROPHE_S_VARIANTS: Final[frozenset[str]] = frozenset({"'s", '’s', '‘s'})
+__all__ = ['OPEN_QUOTES']
 
 # Opening quote characters.
 OPEN_QUOTES: Final[frozenset[str]] = frozenset({'"', '“', "'", '‘'})
