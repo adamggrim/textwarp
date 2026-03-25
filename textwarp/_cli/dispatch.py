9984d0f746f4dc12cd39e8a37f7e940e504676af
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-03-25 13:44:25 -0400

    Create and implement _lazy_load_warping in textwarp/_cli/dispatch.py

--- textwarp/_cli/dispatch.py
+++ textwarp/_cli/dispatch.py
@@ -1,31 +1,33 @@
 """A mapping of string inputs to case conversion functions."""
 
+import importlib
 from typing import Callable, Final
 
-from textwarp.warping import (
-    to_camel_case,
-    to_dot_case,
-    to_kebab_case,
-    to_pascal_case,
-    to_snake_case
-)
+
+def _lazy_load_warping(func_name: str) -> Callable[[str], str]:
+    """Lazy load functions from the warping module."""
+    def wrapper(text: str) -> str:
+        mod = importlib.import_module('textwarp.warping')
+        return getattr(mod, func_name)(text)
+    return wrapper
+
 
 __all__ = ['CASE_NAMES_FUNC_MAP']
 
 # Mapping of valid case name inputs to their standardized names.
 CASE_NAMES_FUNC_MAP: Final[dict[str, Callable[[str], str]]] = {
-    'camel': to_camel_case,
-    'camel case': to_camel_case,
-    'dot': to_dot_case,
-    'dot case': to_dot_case,
-    'kebab': to_kebab_case,
-    'kebab case': to_kebab_case,
+    'camel': _lazy_load_warping('to_camel_case'),
+    'camel case': _lazy_load_warping('to_camel_case'),
+    'dot': _lazy_load_warping('to_dot_case'),
+    'dot case': _lazy_load_warping('to_dot_case'),
+    'kebab': _lazy_load_warping('to_kebab_case'),
+    'kebab case': _lazy_load_warping('to_kebab_case'),
     'lower': str.lower,
     'lowercase': str.lower,
-    'pascal': to_pascal_case,
-    'pascal case': to_pascal_case,
-    'snake': to_snake_case,
-    'snake case': to_snake_case,
+    'pascal': _lazy_load_warping('to_pascal_case'),
+    'pascal case': _lazy_load_warping('to_pascal_case'),
+    'snake': _lazy_load_warping('to_snake_case'),
+    'snake case': _lazy_load_warping('to_snake_case'),
     'upper': str.upper,
     'uppercase': str.upper
 }
