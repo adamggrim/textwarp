8ad11d9b81e3ade6fa8c92a290844b3d10aebd4c
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-03-30 01:16:33 -0400

    Create textwarp/_core/providers/en/data/__init__.py

---
+++ textwarp/_core/providers/en/data/__init__.py
@@ -0,0 +1,19 @@
+"""Namespaces for loading English-specific data."""
+
+from textwarp._core.providers.en.data import (
+    contraction_expansion,
+    entity_casing,
+    loader,
+    punctuation,
+    string_casing,
+    token_casing
+)
+
+__all__ = [
+    'contraction_expansion',
+    'entity_casing',
+    'loader',
+    'punctuation',
+    'string_casing',
+    'token_casing'
+]
