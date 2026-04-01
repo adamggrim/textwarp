02f1c2a667203efb3b00ade62a90d10265b9c408
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-01 18:22:44 -0400

    Create textwarp/_core/providers/en/__init__.py

---
+++ textwarp/_core/providers/en/__init__.py
@@ -0,0 +1,29 @@
+"""Exposes English-specific language provider modules."""
+
+from textwarp._core.providers.en import (
+    casing,
+    constants,
+    contractions,
+    data,
+    disambiguation,
+    encoding,
+    handlers,
+    patterns,
+    provider,
+    punctuation,
+    utils,
+)
+
+all = [
+    'casing',
+    'constants',
+    'contractions',
+    'data',
+    'disambiguation',
+    'encoding',
+    'handlers',
+    'patterns',
+    'provider',
+    'punctuation',
+    'utils'
+]
