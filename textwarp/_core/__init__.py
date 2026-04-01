89ff21638614a5b1dcc48d8d3eacda8123936ba0
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-01 18:21:17 -0400

    Update textwarp/_core/__init__.py

--- textwarp/_core/__init__.py
+++ textwarp/_core/__init__.py
@@ -1,8 +1,7 @@
 """Exposes core configuration, constants, and models."""
 
-from textwarp._core.config import *
+from textwarp._core.encoding import *
 from textwarp._core.constants import *
-from textwarp._core.decorators import *
 from textwarp._core.enums import *
 from textwarp._core.exceptions import *
 from textwarp._core.models import *
