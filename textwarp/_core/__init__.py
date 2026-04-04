c7ee191c1c438d2ca5a8207ca4fb12e6b848405a
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-04 00:12:03 -0400

    Use explicit imports in textwarp/_core/__init__.py

--- textwarp/_core/__init__.py
+++ textwarp/_core/__init__.py
@@ -1,9 +1,54 @@
 """Exposes core configuration, constants, and models."""
 
-from textwarp._core.encoding import *
-from textwarp._core.constants import *
-from textwarp._core.enums import *
-from textwarp._core.exceptions import *
-from textwarp._core.models import *
-from textwarp._core.types import *
-from textwarp._core.utils import *
+from textwarp._core.encoding import get_morse_map, get_morse_reversed_map
+from textwarp._core.constants import maps, nlp, patterns
+from textwarp._core.enums import (
+    CaseSeparator,
+    Casing,
+    CountLabels,
+    PresenceCheckType,
+    RegexBoundary
+)
+from textwarp._core.exceptions import (
+    CaseNotFoundError,
+    EmptyClipboardError,
+    InvalidCaseNameError,
+    NoCaseNameError,
+    NoRegexError,
+    NoTextError,
+    RegexNotFoundError,
+    TextNotFoundError,
+    WhitespaceCaseNameError,
+    WhitespaceClipboardError
+)
+from textwarp._core.models import POSCounts, WordCount
+from textwarp._core.types import EntityCasingContext, JSONType
+from textwarp._core.utils import find_first_alphabetical_idx
+
+__all__ = [
+    'get_morse_map',
+    'get_morse_reversed_map',
+    'maps',
+    'nlp',
+    'patterns',
+    'CaseSeparator',
+    'Casing',
+    'CountLabels',
+    'PresenceCheckType',
+    'RegexBoundary',
+    'CaseNotFoundError',
+    'EmptyClipboardError',
+    'InvalidCaseNameError',
+    'NoCaseNameError',
+    'NoRegexError',
+    'NoTextError',
+    'RegexNotFoundError',
+    'TextNotFoundError',
+    'WhitespaceCaseNameError',
+    'WhitespaceClipboardError',
+    'POSCounts',
+    'WordCount',
+    'EntityCasingContext',
+    'JSONType',
+    'find_first_alphabetical_idx'
+]
