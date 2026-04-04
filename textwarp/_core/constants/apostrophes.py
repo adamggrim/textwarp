557613575fb91b2ccd43823bfe7064fb9cea13a9
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-03 23:39:25 -0400

    Delete textwarp/_core/constants/apostrophes.py

--- textwarp/_core/constants/apostrophes.py
+++
@@ -1,8 +0,0 @@
-"""Sets used across the package for grouping apostrophe characters."""
-
-from typing import Final
-
-__all__ = ['OPEN_QUOTES']
-
-# Opening quote characters.
-OPEN_QUOTES: Final[frozenset[str]] = frozenset({'"', '“', "'", '‘'})
