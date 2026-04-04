4ea4091e64f3749419548398f34d5716119d0521
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-04 00:13:01 -0400

    Remove language-specific objects from textwarp/_core/constants/nlp.py

--- textwarp/_core/constants/nlp.py
+++ textwarp/_core/constants/nlp.py
@@ -6,15 +6,7 @@ __all__ = [
     'NOUN_TAGS',
     'POS_TAGS',
     'POS_WORD_TAGS',
-    'PROPER_NOUN_ENTITIES',
-    'SUBJECT_POS_TAGS'
 ]
-
-# Coarse-grained parts-of-speech tags for stopping a subject search when
-# looking left.
-LEFT_SEARCH_STOP_TAGS: Final[frozenset[str]] = frozenset(
-    {'DET', 'VERB', 'PUNCT'}
-)
 
 # Coarse-grained parts-of-speech tags for singular and proper nouns.
 NOUN_TAGS: Final[frozenset[str]] = frozenset({'NOUN', 'PROPN'})
@@ -43,26 +35,3 @@ POS_TAGS: Final[tuple[tuple[str, str], ...]] = (
 POS_WORD_TAGS: Final[tuple[str, ...]] = tuple(
     item[0] for item in POS_TAGS if item[0] != 'X'
 )
-
-# Coarse-grained parts-of-speech tags for stopping a subject search when
-# looking right.
-RIGHT_SEARCH_STOP_TAGS: Final[frozenset[str]] = frozenset({'VERB', 'PUNCT'})
-
-# Named entities that are typically proper nouns.
-PROPER_NOUN_ENTITIES: Final[frozenset[str]] = frozenset({
-    'EVENT',
-    'FAC',
-    'GPE',
-    'LANGUAGE',
-    'LAW',
-    'LOC',
-    'NORP',
-    'ORG',
-    'PERSON',
-    'PRODUCT',
-    'WORK_OF_ART'
-})
-
-# Coarse-grained parts-of-speech tags for pronouns, proper nouns, and
-# nouns.
-SUBJECT_POS_TAGS: Final[frozenset[str]] = frozenset({'PRON', 'PROPN', 'NOUN'})
