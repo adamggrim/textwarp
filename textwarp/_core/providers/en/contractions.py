2a25d3cc2997dead22ed5ad82d00adf79218e347
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-03 23:49:49 -0400

    Create textwarp/_core/providers/en/contractions.py

---
+++ textwarp/_core/providers/en/contractions.py
@@ -0,0 +1,18 @@
+"""Sets used in English contraction variants."""
+
+from typing import Final
+
+__all__ = [
+    'AIN_T_SUFFIX_VARIANTS',
+    'APOSTROPHE_D_VARIANTS',
+    'APOSTROPHE_S_VARIANTS'
+]
+
+# Variants of "'s" for contractions.
+AIN_T_SUFFIX_VARIANTS: Final[frozenset[str]] = frozenset({"n't", 'n’t', 'n‘t'})
+
+# Variants of "'d" for contractions.
+APOSTROPHE_D_VARIANTS: Final[frozenset[str]] = frozenset({"'d", '’d', '‘d'})
+
+# Variants of "'s" for contractions.
+APOSTROPHE_S_VARIANTS: Final[frozenset[str]] = frozenset({"'s", '’s', '‘s'})
