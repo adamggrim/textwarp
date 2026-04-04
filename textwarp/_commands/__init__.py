c04c928de0520064c6bcff0353efcedc00fd2af0
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-04 00:12:17 -0400

    Use explicit imports in textwarp/_commands/__init__.py

--- textwarp/_commands/__init__.py
+++ textwarp/_commands/__init__.py
@@ -2,5 +2,26 @@
 Exposes analysis and replacement commands for use across the package.
 """
 
-from textwarp._commands.analysis import *
-from textwarp._commands.replacement import *
+from textwarp._commands.analysis import (
+    char_count,
+    line_count,
+    mfws,
+    pos_count,
+    sentence_count,
+    time_to_read,
+    word_count
+)
+from textwarp._commands.replacement import replace, replace_case, replace_regex
+
+__all__ = [
+    'char_count',
+    'line_count',
+    'mfws',
+    'pos_count',
+    'sentence_count',
+    'time_to_read',
+    'word_count',
+    'replace',
+    'replace_case',
+    'replace_regex'
+]
