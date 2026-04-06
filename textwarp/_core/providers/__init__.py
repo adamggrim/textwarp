7eb9e69303a80814b1643132283c7068418cab62
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-05 23:53:03 -0400

    Remove EnglishProvider from _core/providers/__init__.py

--- textwarp/_core/providers/__init__.py
+++ textwarp/_core/providers/__init__.py
@@ -3,9 +3,5 @@ Exposes strategy pattern classes containing language-specific logic.
 """
 
 from textwarp._core.providers.base import LanguageProvider
-from textwarp._core.providers.en import EnglishProvider
 
-__all__ = [
-    'LanguageProvider',
-    'EnglishProvider'
-]
+__all__ = ['LanguageProvider']
