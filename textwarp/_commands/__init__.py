b15ab4c316e5dfc71a00b75a37f1f3495457b311
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-06 00:00:40 -0400

    Delete imports from _commands/__init__.py to preserve lazy loading

--- textwarp/_commands/__init__.py
+++ textwarp/_commands/__init__.py
@@ -1,27 +1,5 @@
 """
-Exposes analysis and replacement commands for use across the package.
+Namespace for analysis and replacement commands.
+
+(No imports to preserve lazy loading.)
 """
-
-from textwarp._commands.analysis import (
-    char_count,
-    line_count,
-    mfws,
-    pos_count,
-    sentence_count,
-    time_to_read,
-    word_count
-)
-from textwarp._commands.replacement import replace, replace_case, replace_regex
-
-__all__ = [
-    'char_count',
-    'line_count',
-    'mfws',
-    'pos_count',
-    'sentence_count',
-    'time_to_read',
-    'word_count',
-    'replace',
-    'replace_case',
-    'replace_regex'
-]
