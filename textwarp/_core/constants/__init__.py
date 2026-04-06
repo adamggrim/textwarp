0e26cf284f65df9bf798f8c3b2f08b02c8a86df6
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-05 23:49:09 -0400

    Update _core/constants/__init__.py with explicit imports

--- textwarp/_core/constants/__init__.py
+++ textwarp/_core/constants/__init__.py
@@ -1,5 +1,19 @@
 """Exposes constants for use across the package."""
 
-from textwarp._core.constants.maps import *
-from textwarp._core.constants.nlp import *
-from textwarp._core.constants.patterns import *
+from textwarp._core.constants.maps import get_case_names_regex_map
+from textwarp._core.constants.nlp import NOUN_TAGS, POS_TAGS, POS_WORD_TAGS
+from textwarp._core.constants.patterns import (
+    case_conversion,
+    cases,
+    warping
+)
+
+__all__ = [
+    'get_case_names_regex_map',
+    'NOUN_TAGS',
+    'POS_TAGS',
+    'POS_WORD_TAGS',
+    'case_conversion',
+    'cases',
+    'warping'
+]
