d4bf9c27c8ccf32aaad2d9011049735ac9009fdc
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-09 01:20:23 -0400

    Remove English-specific tests from tests/core/constants/test_nlp.py

--- tests/core/constants/test_nlp.py
+++ tests/core/constants/test_nlp.py
@@ -1,13 +1,9 @@
 """Tests for NLP tag collections and constants."""
 
 from textwarp._core.constants.nlp import (
-    LEFT_SEARCH_STOP_TAGS,
     NOUN_TAGS,
     POS_TAGS,
-    POS_WORD_TAGS,
-    PROPER_NOUN_ENTITIES,
-    RIGHT_SEARCH_STOP_TAGS,
-    SUBJECT_POS_TAGS
+    POS_WORD_TAGS
 )
 
 
@@ -15,11 +11,7 @@ def test_nlp_tag_sets_are_frozensets():
     """
     Verify that the tag collections are immutable `frozenset` objects.
     """
-    assert isinstance(LEFT_SEARCH_STOP_TAGS, frozenset)
     assert isinstance(NOUN_TAGS, frozenset)
-    assert isinstance(PROPER_NOUN_ENTITIES, frozenset)
-    assert isinstance(RIGHT_SEARCH_STOP_TAGS, frozenset)
-    assert isinstance(SUBJECT_POS_TAGS, frozenset)
 
 
 def test_noun_tags():
@@ -27,13 +19,6 @@ def test_noun_tags():
     assert 'NOUN' in NOUN_TAGS
     assert 'PROPN' in NOUN_TAGS
     assert 'VERB' not in NOUN_TAGS
-
-
-def test_proper_noun_entities():
-    """Verify the `PROPER_NOUN_ENTITIES` set."""
-    assert 'PERSON' in PROPER_NOUN_ENTITIES
-    assert 'ORG' in PROPER_NOUN_ENTITIES
-    assert 'WORK_OF_ART' in PROPER_NOUN_ENTITIES
 
 
 def test_pos_tags_structure():
@@ -54,10 +39,3 @@ def test_pos_word_tags():
     assert 'NOUN' in POS_WORD_TAGS
     assert 'VERB' in POS_WORD_TAGS
     assert 'X' not in POS_WORD_TAGS
-
-
-def test_subject_pos_tags():
-    """Verify tags that represent subjects in dependency parsing."""
-    assert 'PRON' in SUBJECT_POS_TAGS
-    assert 'PROPN' in SUBJECT_POS_TAGS
-    assert 'NOUN' in SUBJECT_POS_TAGS
