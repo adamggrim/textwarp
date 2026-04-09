2dabe356f5dae22e21c65bc7c817fcd7142b0ffe
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-08 20:21:52 -0400

    Use get functions for constants in tests/commands/test_replacement.py

--- tests/cli/constants/test_inputs.py
+++ tests/cli/constants/test_inputs.py
@@ -1,36 +1,40 @@
 """Tests for command-line input sets."""
 
-from textwarp._cli.constants.inputs import EXIT_INPUTS, NO_INPUTS, YES_INPUTS
+from textwarp._cli.constants.inputs import (
+    get_exit_inputs,
+    get_no_inputs,
+    get_yes_inputs
+)
 
 
 def test_input_sets_are_frozensets():
     """
     Verify that input collections are immutable `frozenset` objects.
     """
-    assert isinstance(EXIT_INPUTS, frozenset)
-    assert isinstance(NO_INPUTS, frozenset)
-    assert isinstance(YES_INPUTS, frozenset)
+    assert isinstance(get_exit_inputs(), frozenset)
+    assert isinstance(get_no_inputs(), frozenset)
+    assert isinstance(get_yes_inputs(), frozenset)
 
 
 def test_yes_inputs():
     """Test affirmative input values."""
-    assert 'yes' in YES_INPUTS
-    assert 'y' in YES_INPUTS
+    assert 'yes' in get_yes_inputs()
+    assert 'y' in get_yes_inputs()
 
 
 def test_no_inputs():
     """Test negative input values."""
-    assert 'no' in NO_INPUTS
-    assert 'n' in NO_INPUTS
+    assert 'no' in get_no_inputs()
+    assert 'n' in get_no_inputs()
 
 
 def test_exit_inputs():
     """Test exit input values."""
-    assert 'quit' in EXIT_INPUTS
-    assert 'exit' in EXIT_INPUTS
-    assert 'q' in EXIT_INPUTS
-    assert 'e' in EXIT_INPUTS
-    assert 'exit, pursued by a bear' not in EXIT_INPUTS
+    assert 'quit' in get_exit_inputs()
+    assert 'exit' in get_exit_inputs()
+    assert 'q' in get_exit_inputs()
+    assert 'e' in get_exit_inputs()
+    assert 'exit, pursued by a bear' not in get_exit_inputs()
 
 
 def test_inputs_are_mutually_exclusive():
@@ -38,12 +42,12 @@ def test_inputs_are_mutually_exclusive():
     Ensure there is no overlap between affirmative, negative, and exit
     commands.
     """
-    assert YES_INPUTS.isdisjoint(NO_INPUTS), (
+    assert get_yes_inputs().isdisjoint(get_no_inputs()), (
         'Overlap between YES and NO inputs.'
     )
-    assert YES_INPUTS.isdisjoint(EXIT_INPUTS), (
+    assert get_yes_inputs().isdisjoint(get_exit_inputs()), (
         'Overlap between YES and EXIT inputs.'
     )
-    assert NO_INPUTS.isdisjoint(EXIT_INPUTS), (
+    assert get_no_inputs().isdisjoint(get_exit_inputs()), (
         'Overlap between NO and EXIT inputs.'
     )
