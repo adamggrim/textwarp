9d0a9651018be11fddeb6faba740d9226de49b31
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-09 20:08:38 -0400

    Delete _load_universal function to reduce code duplication

--- textwarp/_core/encoding.py
+++ textwarp/_core/encoding.py
@@ -1,21 +1,11 @@
 """Functions for loading universal encoding data."""
 
-import importlib.resources
-import json
 from functools import lru_cache
 from types import MappingProxyType
 from typing import Mapping, cast
-from pathlib import Path
 
-from textwarp._core.types import JSONType
 
 __all__ = ['get_morse_map', 'get_morse_reversed_map']
-
-
-def _load_universal(relative_path: str | Path) -> JSONType:
-    pkg_files = importlib.resources.files(__package__)
-    resource = pkg_files.joinpath('data', *Path(relative_path).parts)
-    return cast(JSONType, json.loads(resource.read_text(encoding='utf-8')))
 
 
 @lru_cache(maxsize=1)
