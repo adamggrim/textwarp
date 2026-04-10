96f008a81aa98b3ffec29d7cf5276dbd9717d198
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-09 20:28:56 -0400

    Use new data loading function in punctuation.py

--- textwarp/_core/providers/en/data/punctuation.py
+++ textwarp/_core/providers/en/data/punctuation.py
@@ -1,13 +1,19 @@
 """Functions for loading English punctuation rules."""
 
+from functools import lru_cache
 from typing import cast
 
-from textwarp._core.providers.en.data.loader import load_data
+from textwarp._core.utils import load_json_data
 
 __all__ = ['get_elision_words']
 
 
+@lru_cache(maxsize=1)
 def get_elision_words() -> frozenset[str]:
     """Get a cached `frozenset` of elision words."""
     return frozenset(
-        cast(list[str], load_data('elision_words.json')))
+        cast(
+            list[str],
+            load_json_data('elision_words.json', locale='en')
+        )
+    )
