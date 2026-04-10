4e8997f6fd7e6873418148ec61d73a2e3f4eb4be
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-09 20:17:44 -0400

    Add locale parameter to revised loading function

--- textwarp/_core/providers/en/data/string_casing.py
+++ textwarp/_core/providers/en/data/string_casing.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from types import MappingProxyType
 from typing import Final, Mapping, cast
 
-from textwarp._core.providers.en.data.loader import load_data
+from textwarp._core.utils import load_json_data
 
 __all__ = [
     'get_lookup_map',
@@ -22,14 +22,14 @@ DIR: Final = Path('string_casing')
 
 @lru_cache(maxsize=1)
 def get_lookup_map() -> Mapping[str, str]:
-    """Get a combined cached mapping for string casings."""
+    """Get a combined cached map for string casings."""
     absolute_map = cast(
         dict[str, str],
-        load_data(DIR / 'absolute_casings_map.json')
+        load_json_data(DIR / 'absolute_casings_map.json', locale='en')
     )
     prefixed_surnames_map = cast(
         dict[str, str],
-        load_data(DIR / 'prefixed_surnames_map.json')
+        load_json_data(DIR / 'prefixed_surnames_map.json', locale='en')
     )
     return MappingProxyType(
         {**prefixed_surnames_map, **absolute_map}
@@ -41,8 +41,8 @@ def get_lowercase_abbreviations() -> frozenset[str]:
     """
     Get a cached `frozenset` of lowercase abbreviations.
     """
-    return frozenset(cast(list[str], load_data(
-        DIR / 'lowercase_abbreviations.json'
+    return frozenset(cast(list[str], load_json_data(
+        DIR / 'lowercase_abbreviations.json', locale='en'
     )))
 
 
@@ -52,7 +52,7 @@ def get_map_suffix_exceptions() -> frozenset[str]:
     return frozenset(
         cast(
             list[str],
-            load_data(DIR / 'map_suffix_exceptions.json')
+            load_json_data(DIR / 'map_suffix_exceptions.json', locale='en')
         )
     )
 
@@ -63,7 +63,7 @@ def get_surname_prefix_exceptions() -> frozenset[str]:
     return frozenset(
         cast(
             list[str],
-            load_data(DIR / 'surname_prefix_exceptions.json')
+            load_json_data(DIR / 'surname_prefix_exceptions.json', locale='en')
         )
     )
 
@@ -74,6 +74,6 @@ def get_surname_prefixes() -> frozenset[str]:
     return frozenset(
         cast(
             list[str],
-            load_data(DIR / 'surname_prefixes.json')
+            load_json_data(DIR / 'surname_prefixes.json', locale='en')
         )
     )
