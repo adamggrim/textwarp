b61cfba5da9e25698bb08eabd755219fc4f7a365
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-15 17:20:40 -0400

    Replace bespoke lazy loading function in textwarp/_cli/dispatch.py

--- textwarp/_cli/dispatch.py
+++ textwarp/_cli/dispatch.py
@@ -1,33 +1,25 @@
 """A map of string inputs to case conversion functions."""
 
-import importlib
 from typing import Callable, Final
 
-
-def _lazy_load_warping(func_name: str) -> Callable[[str], str]:
-    """Lazy load functions from the warping module."""
-    def wrapper(text: str) -> str:
-        mod = importlib.import_module('textwarp.warping')
-        return getattr(mod, func_name)(text)
-    return wrapper
-
+from textwarp._cli.args import _lazy_load
 
 __all__ = ['CASE_NAMES_FUNC_MAP']
 
 # Map of valid case name inputs and their associated functions.
 CASE_NAMES_FUNC_MAP: Final[dict[str, Callable[[str], str]]] = {
-    'camel': _lazy_load_warping('to_camel_case'),
-    'camel case': _lazy_load_warping('to_camel_case'),
-    'dot': _lazy_load_warping('to_dot_case'),
-    'dot case': _lazy_load_warping('to_dot_case'),
-    'kebab': _lazy_load_warping('to_kebab_case'),
-    'kebab case': _lazy_load_warping('to_kebab_case'),
+    'camel': _lazy_load('..warping', 'to_camel_case'),
+    'camel case': _lazy_load('..warping', 'to_camel_case'),
+    'dot': _lazy_load('..warping', 'to_dot_case'),
+    'dot case': _lazy_load('..warping', 'to_dot_case'),
+    'kebab': _lazy_load('..warping', 'to_kebab_case'),
+    'kebab case': _lazy_load('..warping', 'to_kebab_case'),
     'lower': str.lower,
     'lowercase': str.lower,
-    'pascal': _lazy_load_warping('to_pascal_case'),
-    'pascal case': _lazy_load_warping('to_pascal_case'),
-    'snake': _lazy_load_warping('to_snake_case'),
-    'snake case': _lazy_load_warping('to_snake_case'),
+    'pascal': _lazy_load('..warping', 'to_pascal_case'),
+    'pascal case': _lazy_load('..warping', 'to_pascal_case'),
+    'snake': _lazy_load('..warping', 'to_snake_case'),
+    'snake case': _lazy_load('..warping', 'to_snake_case'),
     'upper': str.upper,
     'uppercase': str.upper
 }
