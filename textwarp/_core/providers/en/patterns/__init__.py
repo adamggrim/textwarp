c1a1d3eb9b18142d7efd532d59a6d4f938789864
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-15 20:10:15 -0400

    Init file alphabetizations

--- textwarp/_core/providers/en/patterns/__init__.py
+++ textwarp/_core/providers/en/patterns/__init__.py
@@ -1,16 +1,16 @@
 """Exposes English-specific regular expression patterns."""
 
 from textwarp._core.providers.en.patterns.warping import (
-    get_apostrophe_in_word,
     get_ambiguous_contraction,
+    get_apostrophe_in_word,
     get_cardinal,
+    get_common_stateless_participles,
     get_contraction,
     get_contraction_suffixes_pattern,
-    get_common_stateless_participles,
     get_idiomatic_phrases,
     get_map_suffix_exceptions_pattern,
-    get_name_prefix_exception_pattern,
     get_n_t_suffix,
+    get_name_prefix_exception_pattern,
     get_ordinal,
     get_punct_inside,
     get_punct_outside,
@@ -20,16 +20,16 @@ from textwarp._core.providers.en.patterns.warping import (
 )
 
 __all__ = [
-    'get_apostrophe_in_word',
     'get_ambiguous_contraction',
+    'get_apostrophe_in_word',
     'get_cardinal',
+    'get_common_stateless_participles',
     'get_contraction',
     'get_contraction_suffixes_pattern',
-    'get_common_stateless_participles',
     'get_idiomatic_phrases',
     'get_map_suffix_exceptions_pattern',
-    'get_name_prefix_exception_pattern',
     'get_n_t_suffix',
+    'get_name_prefix_exception_pattern',
     'get_ordinal',
     'get_punct_inside',
     'get_punct_outside',
