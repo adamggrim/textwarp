70bdfc1e5cce886d8a9ca75ef5e67b9126885be6
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-17 12:00:46 -0400

    Replace test_warping.py with test_patterns.py

--- tests/core/providers/en/test_patterns.py
+++ tests/core/providers/en/test_patterns.py
@@ -1,10 +1,8 @@
 """Tests for English-specific regular expressions."""
 
-from textwarp._core.providers.en.patterns.warping import (
+from textwarp._core.providers.en.patterns import (
     get_apostrophe_in_word,
-    get_cardinal,
-    get_n_t_suffix,
-    get_ordinal
+    get_n_t_suffix
 )
 
 
@@ -18,24 +16,6 @@ def test_get_apostrophe_in_word():
     assert match is not None
     assert match.group(0) == "'"
     assert pattern.search("'n'") is not None
-
-
-def test_get_cardinal():
-    """Test the regular expression matching cardinal numbers."""
-    pattern = get_cardinal()
-    match = pattern.search('The year is 1984.')
-    assert match is not None
-    assert match.group(0) == '1984'
-    assert pattern.search('1984th') is None
-
-
-def test_get_ordinal():
-    """Test the regular expression matching ordinal numbers."""
-    pattern = get_ordinal()
-    match = pattern.search('Thursday, May 12th')
-    assert match is not None
-    assert match.group(0) == '12th'
-    assert pattern.search('12') is None
 
 
 def test_get_n_t_suffix():
