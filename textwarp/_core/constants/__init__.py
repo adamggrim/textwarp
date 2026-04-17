c33ac5bd3fec73a1f362bd41b8abb4be8a3d017e
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-17 12:02:37 -0400

    Update textwarp/_core/constants/__init__.py

--- textwarp/_core/constants/__init__.py
+++ textwarp/_core/constants/__init__.py
@@ -3,7 +3,6 @@
 from textwarp._core.constants.maps import get_case_names_regex_map
 from textwarp._core.constants.nlp import NOUN_TAGS, POS_TAGS, POS_WORD_TAGS
 from textwarp._core.constants.patterns import (
-    case_conversion,
     cases,
     warping
 )
@@ -13,7 +12,6 @@ __all__ = [
     'NOUN_TAGS',
     'POS_TAGS',
     'POS_WORD_TAGS',
-    'case_conversion',
     'cases',
     'warping'
 ]
