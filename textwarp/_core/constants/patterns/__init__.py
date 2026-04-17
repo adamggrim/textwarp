ec322d4a25f43bf0b24c970138c065b0067c2550
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-17 12:02:30 -0400

    Update textwarp/_core/constants/patterns/__init__.py

--- textwarp/_core/constants/patterns/__init__.py
+++ textwarp/_core/constants/patterns/__init__.py
@@ -1,11 +1,9 @@
 """Exposes core regular expression patterns."""
 
-from textwarp._core.constants.patterns import case_conversion
 from textwarp._core.constants.patterns import cases
 from textwarp._core.constants.patterns import warping
 
 __all__ = [
-    'case_conversion',
     'cases',
     'warping'
 ]
