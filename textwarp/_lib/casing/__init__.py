9bcec14ab7516b38355d2e41bd321c89fd6aa337
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-04-27 10:13:15 -0400

    Update textwarp/_lib/casing/__init__.py

--- textwarp/_lib/casing/__init__.py
+++ textwarp/_lib/casing/__init__.py
@@ -1,12 +1,12 @@
 """Exposes casing logic for use across the package."""
 
-from textwarp._lib.casing.case_conversion import (
+from textwarp._lib.casing.entity_casing import map_all_entities
+from textwarp._lib.casing.natural_casing import to_natural_case
+from textwarp._lib.casing.programming_casing import (
     to_camel_case,
-    to_natural_case,
     to_pascal_case,
     to_separator_case
 )
-from textwarp._lib.casing.entity_casing import map_all_entities
 from textwarp._lib.casing.string_casing import case_from_string
 from textwarp._lib.casing.token_casing import should_capitalize_pos_or_length
 
